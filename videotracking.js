84d1490b0573c190333c2090442e15950278f0c8
Anthony Castro <anthony.castro@vervemobile.com>
2018-01-08 16:52:33 -0500

    Update tracking logic and add MOAT tracking

--- videotracking.js
+++ videotracking.js
@@ -1,9 +1,73 @@
 Redfive = (function(Redfive) {
   Redfive.Video = Redfive.Video || {};
 
+  function trackVideo(ad, video, duration, object, trackCallback) {
+    window.MOAT = null;
+    // If there are MOAT configuration parameters, then we should implement MOAT tracking
+    if(ad.config.params.moatClientLevel1) {
+      MOAT = initMoatTracking(video.parentElement, {
+        level1: ad.config.params.moatClientLevel1,
+        level2: ad.config.params.moatClientLevel2,
+        level3: ad.config.params.moatClientLevel3,
+        level4: ad.config.params.moatClientLevel4,
+        slicer1: ad.config.params.moatClientSlicer1,
+        slicer2: ad.config.params.moatClientSlicer2
+      }, duration, "verveinappvideo189175689658");
 
-  function trackVideo(ad, video, duration, trackCallback) {
-    var highestInterval = -1;
+      if(ad.interstitial) {
+        ad.interstitial.on("click:close", function() {
+          reportToMoat("AdStopped");
+        });
+      }
+      if(ad.expandable) {
+        ad.expandable.on("click:close", function() {
+          reportToMoat("AdStopped");
+        });
+      }
+      if(window.vrvsdk && window.vrvsdk.subscribeDeviceAttributesListener) {
+        vrvsdk.subscribeDeviceAttributesListener(function(deviceAttributes) {
+          MOAT.dispatchEvent({
+            type: "AdVolumeChange",
+            adVolume: getVideoVolume(),
+            deviceVolume: deviceAttributes.volume
+          });
+        });
+      }
+    }
+
+    function reportToMoat(type) {
+      if(MOAT) {
+        if(window.vrvsdk && window.vrvsdk.getDeviceAttributes) {
+          // fuck it
+          window.type = type;
+          window.getVideoVolume = getVideoVolume;
+          vrvsdk.getDeviceAttributes(function(data) {
+            MOAT.dispatchEvent({
+              type: type,
+              adVolume: getVideoVolume(),
+              deviceVolume: data.deviceAttributes.volume
+            });
+          });
+        }
+        else {
+          MOAT.dispatchEvent({
+            type: type,
+            adVolume: getVideoVolume(),
+            deviceVolume: getVideoVolume()
+          });
+        }
+      }
+    }
+
+    function getVideoVolume() {
+      if(video.muted) {
+        return 0;
+      }
+      return video.volume;
+    }
+
+    var trackedQuartiles = [];
+    var highestMOATInterval = -1;
 
     video.addEventListener("click", function() {
       if(video.paused) {
@@ -14,47 +78,137 @@ Redfive = (function(Redfive) {
       }
     });
 
+    video.addEventListener("volumechange", function(event) {
+      if(MOAT) {
+        reportToMoat("AdVolumeChange");
+      }
+    });
+
     video.addEventListener("play", function() {
-      ad.logger.track("cta_video");
+      if(object) {
+        ad.logger.track("cta_video", {o: object});
+      }
+      else {
+        ad.logger.track("cta_video");
+      }
+      if(MOAT) {
+        reportToMoat("AdVideoPlaying");
+      }
+    });
+
+    var seeking = false;
+    video.addEventListener("seeking", function() {
+      seeking = true;
+    });
+
+    video.addEventListener("seeked", function() {
+      seeking = false;
+      trackedQuartiles = [];
+    });
+
+    video.addEventListener("timeupdate", function() {
+      // Don't fire tracking if user has seeked but video playback hasn't begun
+      if(!video.paused && !seeking) {
+        // We report quartiles as Verve events each time they occur,
+        // even on replays (as indicated in NAT-5091)
+        var currentTime = video.currentTime;
+        // Give some fudge room for video100
+        if(duration - currentTime <= 0.5) {
+          currentTime = duration;
+        }
+
+        var quartile = Math.floor(currentTime / duration * 4) * 25;
+
+        if(trackedQuartiles.indexOf(quartile) === -1) {
+          trackedQuartiles.push(quartile);
+
+          if(trackCallback) {
+            trackCallback(interval);
+          }
+          if(object) {
+            ad.logger.track("video" + quartile, {o: object});
+          }
+          else {
+            ad.logger.track("video" + quartile);
+          }
+        }
+      }
+
+      // MOAT quartile tracking is left intact
+      // Let's skip this logic if MOAT doesn't exist to save some cycles
+      if(MOAT) {
+        // video.played is a collection of ranges
+        // if the user skipped in the video, grab the range with the
+        // longest end point
+        var longest = 0;
+        for(var i = 0; i < video.played.length; i++) {
+          if(video.played.end(i) > longest) {
+            longest = video.played.end(i);
+          }
+        }
+
+        // give some fudge room for video100
+        if(duration - longest <= 0.5) {
+          longest = duration;
+        }
+
+        // longest / duration is a decimal from 0-1
+        // multiply by 4 and floor it to make it 0,1,2,3 or 4
+        // multiply that by 25 and we get our interval
+        var interval = Math.floor(longest / duration * 4) * 25;
+        if(interval > highestMOATInterval) {
+          highestMOATInterval = interval;
+        }
+        else {
+          return;
+        }
+
+        if(interval === 0) {
+          reportToMoat("AdVideoStart");
+        }
+        if(interval === 25) {
+          reportToMoat("AdVideoFirstQuartile");
+        }
+        if(interval === 50) {
+          reportToMoat("AdVideoMidpoint");
+        }
+        if(interval === 75) {
+          reportToMoat("AdVideoThirdQuartile");
+        }
+        if(interval === 100) {
+          reportToMoat("AdVideoComplete");
+        }
+      }
     });
 
     video.addEventListener("pause", function() {
-      // video.played is a collection of ranges
-      // if the user skipped in the video, grab the range with the
-      // longest end point
-      var longest = 0;
-      for(var i = 0; i < video.played.length; i++) {
-        if(video.played.end(i) > longest)
-          longest = video.played.end(i);
-      }
-
-      // give some fudge room for video100
-      if(duration - longest <= 0.5)
-        longest = duration;
-
-      // longest / duration is a decimal from 0-1
-      // multiply by 4 and floor it to make it 0,1,2,3 or 4
-      // multiply that by 25 and we get our interval
-      var interval = Math.floor(longest / duration * 4) * 25;
-
-      // we should only send a given interval once, and if it's the highest
-      if(interval > highestInterval) {
-        highestInterval = interval;
-        if(trackCallback) {
-          trackCallback(interval);
-        }
-        ad.logger.track("video" + interval);
+      if(MOAT) {
+        reportToMoat("AdPaused");
       }
     });
 
     video.addEventListener("ended", function() {
-      highestInterval = -1;
+      trackedQuartiles = [];
+      highestMOATInterval = -1;
     });
+  }
+
+  var trackWithDelayTimer = 0;
+
+  function trackWithDelay(ad, video, duration, callback) {
+    // Exit if we already started tracking
+    if(trackWithDelayTimer) {
+      return;
+    }
+
+    trackWithDelayTimer = setTimeout(function() {
+      trackVideo(ad, video, duration, callback);
+    }, 500);
   }
 
   // Add Verve CTA tracking to video elements
   // if duration isn't specified, pull it from video element
-  // ammo: ad, video, optional duration, optional trackCallback called on each tracking event
+  // ammo: ad, video, optional duration, optional trackCallback called on each tracking event, optional object to add as o: param
   Redfive.Video.track = function(ammo) {
     var video = ammo.video;
     if(!video || !video.play || !video.pause) {
@@ -69,7 +223,7 @@ Redfive = (function(Redfive) {
     var duration;
     if(ammo.duration) {
       duration = parseInt(ammo.duration, 10);
-      trackVideo(ad, video, duration);
+      trackWithDelay(ad, video, duration);
     }
     else {
       duration = video.duration;
@@ -77,15 +231,19 @@ Redfive = (function(Redfive) {
       if(duration === 0 || isNaN(duration)) {
         video.addEventListener("durationchange", function() {
           if(video.duration > 0) {
-            trackVideo(ad, video, video.duration, ammo.trackCallback);
+            trackWithDelay(ad, video, video.duration, ammo.object, ammo.trackCallback);
           }
         });
       }
       else {
-        trackVideo(ad, video, duration, ammo.trackCallback);
+        trackWithDelay(ad, video, duration, ammo.object, ammo.trackCallback);
       }
     }
   };
+
+  // MOAT's tracking init script
+  /*Copyright (c) 2011-2016 Moat Inc. All Rights Reserved.*/
+  function initMoatTracking(a,c,d,h,k){var f=document.createElement("script"),b=[];c={adData:{ids:c,duration:d,url:k},dispatchEvent:function(a){this.sendEvent?(b&&(b.push(a),a=b,b=!1),this.sendEvent(a)):b.push(a)}};d="_moatApi"+Math.floor(1E8*Math.random());var e,g;try{e=a.ownerDocument,g=e.defaultView||e.parentWindow}catch(l){e=document,g=window}g[d]=c;f.type="text/javascript";a&&a.insertBefore(f,a.childNodes[0]||null);f.src="https://z.moatads.com/"+h+"/moatvideo.js#"+d;return c}; // jshint ignore:line
 
   return Redfive;
 })(window["Redfive"] || {});
